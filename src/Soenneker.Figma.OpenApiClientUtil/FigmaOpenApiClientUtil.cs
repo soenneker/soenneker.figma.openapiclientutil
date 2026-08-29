3fad275cf25f93f03bd69c9bba2c460b98f18374
soenneker <jake@soenneker.com>
2026-08-29 14:50:39 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Figma.OpenApiClientUtil/FigmaOpenApiClientUtil.cs
+++ src/Soenneker.Figma.OpenApiClientUtil/FigmaOpenApiClientUtil.cs
@@ -14,7 +14,7 @@ using Soenneker.Utils.AsyncSingleton;
 
 namespace Soenneker.Figma.OpenApiClientUtil;
 
-///<inheritdoc cref="IFigmaOpenApiClientUtil"/>
+/// <inheritdoc cref="IFigmaOpenApiClientUtil"/>
 public sealed class FigmaOpenApiClientUtil : IFigmaOpenApiClientUtil
 {
     private readonly AsyncSingleton<FigmaOpenApiClient> _client;
